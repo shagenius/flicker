3274c7ef96d6393ff478187593af48484e6ca4a9
shagenius <shameemah@gmail.com>
2019-09-25 00:24:13 +1000

    frontent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,63 +8,16 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-<script src="js/app.js" charset="utf-8"></script>        
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script src="js/app.js" charset="utf-8"></script>        
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+         <div class="container">
+             <div class="row">
+                 <div class="col-12">
+                     <h1>Welcome</h1>
+                 </div>
+             </div>
+             <div class="row">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -78,8 +31,17 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
+             </div>
+             <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
                 <div class="row">
                     <form action="/foo/bar" method="POST">
                         @method('PUT')
@@ -89,7 +51,6 @@
                 <div class="links">
                     <?php $echoTest = Flickr::echoThis('helloworld');?>
                 </div>
-            </div>
         </div>
     </body>
 </html>
